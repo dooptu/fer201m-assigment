8111769435001548f57025b5df8440f28d154dae
DoopTu <tuhandsome111@gmail.com>
2023-03-16 23:03:34 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import { ToastContainer } from 'react-toastify';
 import Register from './Components/Registration/Register';
+import Home from './Components/Main/Home';
 
 
 
@@ -15,7 +16,7 @@ function App() {
       <ToastContainer theme='colored' position='top-right'></ToastContainer>
       <BrowserRouter>
         <Routes>
-
+          <Route path='/' element={<Home></Home>}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
 
---
+++ src/Components/Main/Home.js
@@ -0,0 +1,211 @@
+import React, { useEffect, useState, } from "react";
+import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import Header from "../Complement/Header";
+import Footer from "../Complement/Footer";
+import Slider from "../Complement/Slider";
+import './Main.css'
+
+
+
+const URL_WARNING_C = '';
+const URL_WARNING_R = '';
+
+const Home = () => {
+    const [zone, setZone] = useState('A');
+
+    const [username, setUsername] = useState(sessionStorage.getItem('username'));
+    const [id, setId] = useState(sessionStorage.getItem('id'));
+    const [role, setRole] = useState(sessionStorage.getItem('role'));
+    const [URL_WARNING, set_URL_WARNING] = useState('')
+    const [obj, setObj] = useState([]);
+
+    const [showPopupWarning, setShowPopupWarning] = useState(false);
+    const [open, setOpen] = useState();
+
+
+
+    
+
+
+
+
+
+    useEffect(() => {
+        setZone(zone);
+
+    }, [zone]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        sessionStorage.setItem("zone", zone);
+
+        // window.location.href = '/Reservation';
+    }
+
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <title>EParking</title>
+            </Helmet>
+            <Header data={username}></Header>
+            <Slider></Slider>
+
+            {/* -----------------------------zone-area-homepage----------------------- */}
+            <form onSubmit={handleSubmit}>
+
+                <div className="row zone-area-homepage" >
+
+                    <div className="card col-lg-4" style={{ marginLeft: '10%' }}>
+                        <div className="card-header">
+                            <div>
+                                <p style={{ display: 'block', margin: '0 auto', color: "#fff" }}>2.5$ / Day</p>
+                            </div>
+                            <span>Zone A</span>
+                        </div>
+                        <div className="card-body">
+                            <span>Content</span>
+                            <div>
+                                <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>
+                                <b style={{ marginLeft: '10px', fontSize: '12px' }}>Max, Duration: 4 hours</b>
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <form onSubmit={handleSubmit}>
+
+                                <Link to={'/ZoneDetail/A'}>
+
+                                    <button style={{ color: "#fff", width: '25%' }}  ><span>Details</span></button>
+                                </Link>
+                                <Link to={'/Reservation'}>
+                                    <button style={{ color: "#fff" }} onClick={e => setZone(e.target.value)} value='A'><span>Make Reservation</span></button>
+                                </Link>
+                            </form>
+                        </div>
+                    </div>
+
+
+                    <div className="card col-lg-4">
+                        <div className="card-header">
+                            <div>
+                                <p style={{ display: 'block', margin: '0 auto', color: "#fff" }}>2.5$ / Day</p>
+                            </div>
+                            <span>Zone B</span>
+                        </div>
+                        <div className="card-body">
+                            <span>Content</span>
+                            <div>
+                                <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>
+                                <b style={{ marginLeft: '10px', fontSize: '12px' }}>Max, Duration: 4 hours</b>
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <form onSubmit={handleSubmit}>
+
+                            </form>
+
+                            <Link to={'/ZoneDetail/B'}>
+
+                                <button style={{ color: "#fff", width: '25%' }} onClick={() => setZone('B')} value="B"><span>Details</span></button>
+                            </Link>
+                            <Link to={'/Reservation'}>
+                                <button style={{ color: "#fff" }} onClick={e => setZone(e.target.value)} value='B'><span>Make Reservation</span></button>
+                            </Link>
+                        </div>
+                    </div>
+
+
+                    <div className="card col-lg-4">
+                        <div className="card-header">
+                            <div>
+                                <p style={{ display: 'block', margin: '0 auto', color: "#fff" }}>2.5$ / Day</p>
+                            </div>
+                            <span>Zone C</span>
+                        </div>
+                        <div className="card-body">
+                            <span>Content</span>
+                            <div>
+                                <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>
+                                <b style={{ marginLeft: '10px', fontSize: '12px' }}>Max, Duration: 4 hours</b>
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <form onSubmit={handleSubmit}>
+
+                            </form>
+                            <Link to={'/ZoneDetail/C'}>
+
+                                <button style={{ color: "#fff", width: '25%' }} onClick={() => setZone('C')} value="C"><span>Details</span></button>
+                            </Link>
+                            <Link to={'/Reservation'}>
+                                <button style={{ color: "#fff" }} onClick={e => setZone(e.target.value)} value='C'><span>Make Reservation</span></button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    {/* -----------------------------Entry/barrier system & QR code----------------------- */}
+
+                    <div class=" row barrier-homepage" >
+
+                        <div class=" col-lg-6" style={{ marginLeft: '10%' }}>
+                            <h2>
+                                Entry/barrier system & QR code
+                            </h2>
+                            <span>
+                                <h5 style={{ display: 'block', marginBottom: '30px' }}>You can integrate planyo with any automated entry system. Check out our already existing integration with Spartime.</h5>
+
+                                <h5>Your staff can use our mobile app and read the QR code upon entry for speeding up the arrivals.</h5>
+                            </span>
+
+                        </div>
+
+                        <div class="col-lg-6 img-gate-homepage">
+                            <button style={{ color: "#fff" }} type="submit"><span>Make Reservation</span></button>
+                        </div>
+
+
+
+                    </div>
+
+                    {/* -----------------------------Entry/barrier system & QR code----------------------- */}
+
+                    <div class=" row barrier-homepage" >
+
+                        <div class="col-lg-6 img-gate-homepage" style={{ marginLeft: '10%' }}>
+                            <button style={{ color: "#fff" }} type="submit"><span>Make Reservation</span></button>
+                        </div>
+
+                        <div class=" col-lg-6" >
+                            <h2>
+                                Entry/barrier system & QR code
+                            </h2>
+                            <span>
+                                <h5 style={{ display: 'block', marginBottom: '30px' }}>You can integrate planyo with any automated entry system. Check out our already existing integration with Spartime.</h5>
+
+                                <h5>Your staff can use our mobile app and read the QR code upon entry for speeding up the arrivals.</h5>
+                            </span>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </form>
+
+            <Footer></Footer>
+
+            
+
+
+        </HelmetProvider>
+    );
+}
+
+export default Home;
+
+
+
