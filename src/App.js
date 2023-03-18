3ef80601b4e45e51b325c05f50e6eceaf5e889a5
DoopTu <tuhandsome111@gmail.com>
2023-03-18 13:12:33 +0700

    Update folder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Login from './Components/Login/Login';
 import { ToastContainer } from 'react-toastify';
 import Register from './Components/Registration/Register';
 import Home from './Components/Main/Home';
+import Reservation from './Components/Payment/Reservation';
+import ReservationDetail from './Components/Payment/ReservationDetail';
+import PaymentInformation from './Components/Payment/PaymentInformation';
+import ReservationComplete from './Components/Payment/ReservationComplete';
+import Banking from './Components/Payment/Banking';
 
 
 
@@ -19,6 +24,11 @@ function App() {
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
+          <Route path='/Reservation' element = {<Reservation></Reservation>}></Route>
+          <Route path='/ReservationDetail' element={<ReservationDetail></ReservationDetail>}></Route>
+          <Route path='/PaymentInformation' element={<PaymentInformation></PaymentInformation>}></Route>
+          <Route path='/Banking' element={<Banking></Banking>}></Route>
+          <Route path='/ReservationComplete' element={<ReservationComplete></ReservationComplete>}></Route>
 
 
 
