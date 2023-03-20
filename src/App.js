8629bc092712a12b6591e57becbd3af7be61aa71
DoopTu <tuhandsome111@gmail.com>
2023-03-20 09:35:23 +0700

    replace all file

--- src/App.js
+++ src/App.js
@@ -13,10 +13,27 @@ import ReservationComplete from './Components/Payment/ReservationComplete';
 import Banking from './Components/Payment/Banking';
 >>>>>>> f9287d0a3d11d9d46dc232a998df33cd802d29cb
 
+import AccountInformation from './Components/Complement/Tam/Account/AccountInformation';
+import HistoryBooking from './Components/Complement/Tam/Account/HistoryBooking';
+import CompletedBooking from './Components/Complement/Tam/Account/CompletedBooking';
+import CancelledBooking from './Components/Complement/Tam/Account/CancelledBooking';
+import AllBooking from './Components/Complement/Tam/Account/AllBooking';
+import ProfileSetting from './Components/Complement/Tam/Account/ProfileSetting';
 
 
+<<<<<<< HEAD
+import About from './Components/Complement/Tri/About'
+import News from './Components/Complement/Tri/News'
+=======
+tomlenek
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import Payment from './pages/Payment';
+// import Employee from './pages/Employee';
+// import Profile from './pages/Profile';
+// import  Messages  from './pages/Messages';
 
-
+>>>>>>> 25318627db67c8a54a87dfeff0bb9ab24d9f826f
 
 
 function App() {
@@ -25,11 +42,23 @@ function App() {
       <ToastContainer theme='colored' position='top-right'></ToastContainer>
       <BrowserRouter>
         <Routes>
+          <Route path='/allbooking' element={<AllBooking />}></Route>
+          <Route path='/pastbooking' element={<HistoryBooking />}></Route>
+          <Route path='/completedbooking' element={<CompletedBooking />}></Route>
+          <Route path='/cancelledbooking' element={<CancelledBooking />}></Route>
+          <Route path='/profilesetting' element={<ProfileSetting />}></Route>
+          <Route path='/account' element={<AccountInformation />}></Route>
+
+{/* -------------------------------- Tri - About - News ------------------------------------------ */}
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/news' element={<News />}></Route>
+{/* ---------------------------------------------------------------------------------------------- */}
+
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
 <<<<<<< HEAD
-          <Route path='/Reservation' element={<Main />}></Route>
+  <Route path='/Reservation' element={<Main />}></Route>
 =======
           <Route path='/Reservation' element = {<Reservation></Reservation>}></Route>
           <Route path='/ReservationDetail' element={<ReservationDetail></ReservationDetail>}></Route>
@@ -42,10 +71,23 @@ function App() {
 
 >>>>>>> f9287d0a3d11d9d46dc232a998df33cd802d29cb
 
-        </Routes>
-      </BrowserRouter>
+        </Routes >
+      </BrowserRouter >
 
-    </div>
+    </div >
+
+tomlenek
+{/* <div className='App'>     
+      <Navbar />
+      <Routes>                 
+          <Route path='/' element={<Home />}></Route>
+          <Route exact path='/employee' element={<Employee />}></Route>
+          <Route exact path='/payment' element={<Payment />}></Route> 
+          <Route exact path='/profile' element={<Profile />}></Route>
+          <Route exact path='/messages' element={<Messages />}></Route>     
+      </Routes>    
+      </div> */}
+
   );
 }
 
---
+++ src/Components/Complement/Tom/pages/Employee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Employee() {
+  return (
+    <div className='employee'>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default Employee;
