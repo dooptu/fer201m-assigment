3dcd6f786cd07e70c3439da240a0fc84438fd939
DoopTu <tuhandsome111@gmail.com>
2023-03-21 20:03:02 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import Login from './Components/Login/Login';
 import { ToastContainer } from 'react-toastify';
 import Register from './Components/Registration/Register';
 import Home from './Components/Main/Home';
-<<<<<<< HEAD
+
 import Main from './Components/Complement/Khoi/Main_Khoi';
-=======
 import Reservation from './Components/Payment/Reservation';
 import ReservationDetail from './Components/Payment/ReservationDetail';
 import PaymentInformation from './Components/Payment/PaymentInformation';
 import ReservationComplete from './Components/Payment/ReservationComplete';
 import Banking from './Components/Payment/Banking';
->>>>>>> f9287d0a3d11d9d46dc232a998df33cd802d29cb
+
 
 import AccountInformation from './Components/Complement/Tam/Account/AccountInformation';
 import HistoryBooking from './Components/Complement/Tam/Account/HistoryBooking';
@@ -21,19 +20,18 @@ import AllBooking from './Components/Complement/Tam/Account/AllBooking';
 import ProfileSetting from './Components/Complement/Tam/Account/ProfileSetting';
 
 
-<<<<<<< HEAD
+
 import About from './Components/Complement/Tri/About'
 import News from './Components/Complement/Tri/News'
-=======
-tomlenek
-// import Navbar from './components/Navbar';
-// import Home from './pages/Home';
-// import Payment from './pages/Payment';
-// import Employee from './pages/Employee';
-// import Profile from './pages/Profile';
-// import  Messages  from './pages/Messages';
 
->>>>>>> 25318627db67c8a54a87dfeff0bb9ab24d9f826f
+
+import Navbar from './Components/Complement/Tom/components/Navbar';
+
+import Payment from './Components/Complement/Tom/pages/Payment';
+import Employee from './Components/Complement/Tom/pages/Employee';
+import Profile from './Components/Complement/Tom/pages/Profile';
+import  Messages  from './Components/Complement/Tom/pages/Messages';
+
 
 
 function App() {
@@ -49,44 +47,51 @@ function App() {
           <Route path='/profilesetting' element={<ProfileSetting />}></Route>
           <Route path='/account' element={<AccountInformation />}></Route>
 
-{/* -------------------------------- Tri - About - News ------------------------------------------ */}
+          {/* -------------------------------- Tri - About - News ------------------------------------------ */}
           <Route path='/about' element={<About />}></Route>
           <Route path='/news' element={<News />}></Route>
-{/* ---------------------------------------------------------------------------------------------- */}
+          {/* ---------------------------------------------------------------------------------------------- */}
 
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
-<<<<<<< HEAD
-  <Route path='/Reservation' element={<Main />}></Route>
-=======
-          <Route path='/Reservation' element = {<Reservation></Reservation>}></Route>
+
+          <Route path='/Reservation' element={<Main />}></Route>
+
+          <Route path='/Reservation' element={<Reservation></Reservation>}></Route>
           <Route path='/ReservationDetail' element={<ReservationDetail></ReservationDetail>}></Route>
           <Route path='/PaymentInformation' element={<PaymentInformation></PaymentInformation>}></Route>
           <Route path='/Banking' element={<Banking></Banking>}></Route>
           <Route path='/ReservationComplete' element={<ReservationComplete></ReservationComplete>}></Route>
 
+          <Route path='/News' element={<News></News>}></Route>
+          <Route path='/AboutUs' element={<About></About>}></Route>
+
+
+          <Route path='/Employee' element={<Employee></Employee>}></Route>
+          <Route path='/Message' element={<Messages></Messages>}></Route>
+          <Route path='/Payment' element={<Payment></Payment>}></Route>
+          <Route path='/Profile' element={<Profile></Profile>}></Route>
 
 
 
->>>>>>> f9287d0a3d11d9d46dc232a998df33cd802d29cb
 
-        </Routes >
-      </BrowserRouter >
+        </Routes>
+      </BrowserRouter>
 
-    </div >
+    </div>
 
-tomlenek
-{/* <div className='App'>     
-      <Navbar />
-      <Routes>                 
-          <Route path='/' element={<Home />}></Route>
-          <Route exact path='/employee' element={<Employee />}></Route>
-          <Route exact path='/payment' element={<Payment />}></Route> 
-          <Route exact path='/profile' element={<Profile />}></Route>
-          <Route exact path='/messages' element={<Messages />}></Route>     
-      </Routes>    
-      </div> */}
+
+    /* <div className='App'>     
+          <Navbar />
+          <Routes>                 
+              <Route path='/' element={<Home />}></Route>
+              <Route exact path='/employee' element={<Employee />}></Route>
+              <Route exact path='/payment' element={<Payment />}></Route> 
+              <Route exact path='/profile' element={<Profile />}></Route>
+              <Route exact path='/messages' element={<Messages />}></Route>     
+          </Routes>    
+          </div> */
 
   );
 }
