b3b7ab703bd710590229b9dff0b92fe168f13b44
triminhkieu <triminhkieuse@gmail.com>
2023-03-21 20:59:30 +0700

    fix pag and slider

--- src/Components/Complements/Slider.js
+++ src/Components/Complements/Slider.js
@@ -57,7 +57,30 @@ const Slider = () => {
                         <label>Start time *</label>
                         <br />
                         <select class="form-select" autoComplete="" onChange={(e) => setStartTime(e.target.value)}>
-                            {/* wait */}
+                            <option>00:00</option>
+                            <option>01:00</option>
+                            <option>02:00</option>
+                            <option>03:00</option>
+                            <option>04:00</option>
+                            <option>05:00</option>
+                            <option>06:00</option>
+                            <option>07:00</option>
+                            <option>08:00</option>
+                            <option>09:00</option>
+                            <option>10:00</option>
+                            <option>11:00</option>
+                            <option>12:00</option>
+                            <option>13:00</option>
+                            <option>14:00</option>
+                            <option>15:00</option>
+                            <option>16:00</option>
+                            <option>17:00</option>
+                            <option>18:00</option>
+                            <option>19:00</option>
+                            <option>20:00</option>
+                            <option>21:00</option>
+                            <option>22:00</option>
+                            <option>23:00</option>
                         </select>
                     </div>
                     <div className="class-input">
@@ -72,7 +95,29 @@ const Slider = () => {
                         <label>End time *</label>
                         <br />
                         <select class="form-select" autoComplete="off" onChange={(e) => setEndTime(e.target.value)}>
-                            {/* wait */}
+                        <option>00:00</option>
+                            <option>01:00</option>
+                            <option>02:00</option>
+                            <option>03:00</option>
+                            <option>04:00</option>
+                            <option>05:00</option>
+                            <option>06:00</option>
+                            <option>07:00</option>
+                            <option>08:00</option>
+                            <option>09:00</option>
+                            <option>11:00</option>
+                            <option>12:00</option>
+                            <option>13:00</option>
+                            <option>14:00</option>
+                            <option>15:00</option>
+                            <option>16:00</option>
+                            <option>17:00</option>
+                            <option>18:00</option>
+                            <option>19:00</option>
+                            <option>20:00</option>
+                            <option>21:00</option>
+                            <option>22:00</option>
+                            <option>23:00</option>
                         </select>
                     </div>
 
