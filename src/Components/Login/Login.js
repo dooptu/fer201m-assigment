a152f422eb382082cc14ef9bf23874e0cf6b2916
UncleBonn <bonnmyname@gmail.com>
2023-03-21 20:56:27 +0700

    update login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,19 +6,18 @@ import { faCheck, faTimes, faInfoCircle, faUser } from "@fortawesome/free-solid-
 import BackgroundCommon from "../Complement/BackgroundCommon";
 import Helmet from "react-helmet";
 import { HelmetProvider } from "react-helmet-async";
-
-import './Login.css'
-
+import { url_api } from "../../API/api"
 
 // const LOGIN_URL = "https://0c1a-42-118-112-251.ap.ngrok.io/ParkingManagement/api/user/getUser/";
 
-const LOGIN_URL = ''
+const LOGIN_URL = url_api + "/loginAccount?"
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,30}/;
 const EMAIL_REGEX = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
 const Login = () => {
     const [username, usernameupdate] = useState('');
     const [password, passwordupdate] = useState('');
+    const [role, setRole] = useState('');
 
     const usenavigate = useNavigate();
 
@@ -41,7 +40,7 @@ const Login = () => {
         if (validate()) {
             ///implentation
             console.log(username)
-            fetch(LOGIN_URL + "username="+username +"&password=" + password, {
+            fetch(LOGIN_URL + "username=" + username + "&password=" + password, {
                 headers: {
                     method: 'GET',
                     "X-Requested-With": "XMLHttpRequest",
@@ -50,11 +49,11 @@ const Login = () => {
                     "Cache-Control": "no-cache",
                     mode: 'cors'
                 }
-                
+
             }).then((res) => {
                 console.log(res.json)
                 return res.json();
-                
+
             }).then((resp) => {
                 console.log(resp.password)
                 console.log(resp)
@@ -62,23 +61,35 @@ const Login = () => {
                     toast.error('Please Enter valid username');
                 } else {
                     if (resp.password === password) {
-                        console.log(resp);
-                        toast.success(resp.message);
                         sessionStorage.setItem('username', username);
                         sessionStorage.setItem('fullname', resp.fullname);
                         sessionStorage.setItem('email', resp.email);
                         sessionStorage.setItem('phone', resp.phone);
                         sessionStorage.setItem('id', username);
-                        window.location.href ='/'
+                        localStorage.setItem('id', username);
+                        console.log(resp);
+                        window.location.href = '/'
+                        if (resp.message === 'Login Customer Successfully') {
+                            sessionStorage.setItem('role', 'C');
+                            toast.success(resp.message);
+                            window.location.href = '/'
+                        } else
+                            if (resp.message === 'Login Resident Successfully') {
+                                console.log(resp);
+                                toast.success(resp.message);
+                                sessionStorage.setItem('role', 'R');
+                                window.location.href = '/'
+                            }
                         usenavigate('/');
-                    } else {
+                    }
+                    else {
                         toast.error('Please Enter Correct Password');
                     }
                 }
             })
-            .catch((err) => {
-                toast.error('Login Failed due to :' + err.message);
-            });
+                .catch((err) => {
+                    toast.error('Login Failed due to :' + err.message);
+                });
         }
     }
 
@@ -141,11 +152,13 @@ const Login = () => {
                 </Helmet>
                 <div className="container-login">
 
-                    <BackgroundCommon></BackgroundCommon>
-                    
+                    {/* <div className="container-background">
+                <img style={{position:"absolute",zIndex:"10", marginLeft:"-200px", marginTop:"110px"}} src="man.png"></img>
+            </div> */}
 
                     <div className="login-form">
-                        <h2 style={{}}>User Log in</h2>
+                        <BackgroundCommon></BackgroundCommon>
+                        <h2>User Log in</h2>
                         <span style={{ marginBottom: "40px", display: "block" }}>Log in to your YourParkingSpace account.</span>
                         <button className="google" style={{ marginBottom: "10px" }} type="submit"> <img style={{ width: "30px", position: "absolute", left: "20px", marginRight: "30px" }} src='./assets/img/Google_Logo.png' /> Login with Google</button>
                         <button className="google" style={{ marginTop: "0px" }} type="submit"> <img style={{ width: "30px", position: "absolute", left: "20px", marginRight: "30px" }} src='./assets/img/facebook.png' /> Login with Facebook</button>
@@ -155,18 +168,31 @@ const Login = () => {
                             <input placeholder="User Name" style={{ marginTop: "30px" }} value={username} onChange={e => usernameupdate(e.target.value)} ></input>
                             <input placeholder="Password" type="password" value={password} onChange={e => passwordupdate(e.target.value)} ></input>
                             <button style={{ color: "#fff" }} type="submit">Log In</button>
-
                             <Link to={'/Forgotten-pwd'}>Forgotten your password?</Link>
-
                             <br />
                             <div className="anyaccount" ><span>Don’t have any account?</span></div>
                             <Link to={'/register'}> <button className="btn-signup"> <FontAwesomeIcon icon={faUser} style={{ marginRight: "10px" }} />Sign Up for free</button></Link>
-
                         </form>
-
                     </div>
 
-
+                    {/* responsive  */}
+                    <div className="res-form">
+                        <h2>User Log in</h2>
+                        <span style={{ marginBottom: "40px", display: "block" }}>Log in to your YourParkingSpace account.</span>
+                        <button className="google" style={{ marginBottom: "10px" }} type="submit"> <img style={{ width: "30px", position: "absolute", left: "20px", marginLeft: "-40px" }} src='./assets/img/Google_Logo.png' /> Login with Google</button>
+                        <button className="google" style={{ marginTop: "0px" }} type="submit"> <img style={{ width: "30px", position: "absolute", left: "20px", marginLeft: "-40px" }} src='./assets/img/facebook.png' /> Login with Facebook</button>
+                        <br />
+                        <div className="dash-or"><span>Or</span></div>
+                        <form onSubmit={ProceedLogin}>
+                            <input placeholder="User Name" style={{ marginTop: "30px" }} value={username} onChange={e => usernameupdate(e.target.value)} ></input>
+                            <input placeholder="Password" type="password" value={password} onChange={e => passwordupdate(e.target.value)} ></input>
+                            <button style={{ color: "#fff" }} type="submit">Log In</button>
+                            <Link to={'/Forgotten-pwd'} style={{marginLeft:"-30px", color:"#2DC98A"}}>Forgotten your password?</Link>
+                            <br />
+                            <div className="anyaccount" ><span>Don’t have any account?</span></div>
+                            <Link to={'/register'}> <button className="btn-signup"> <FontAwesomeIcon icon={faUser} style={{ marginRight: "10px" }} />Sign Up for free</button></Link>
+                        </form>
+                    </div>
                 </div>
 
             </div>
